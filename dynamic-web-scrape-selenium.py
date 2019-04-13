272949e66970ba6b5c362382314c1d63f5087776
ghigbie <gehigbie@gmail.com>
2019-04-13 07:52:17 -0600

    Generate output

--- dynamic-web-scrape-selenium.py
+++ dynamic-web-scrape-selenium.py
@@ -4,4 +4,5 @@ url = "https://finance.yahoo.com/quote/AAPL/key-statistics?p=AAPL"
 
 browser = webdriver.PhantomJS(executable_path = '/usr/local/bin/PhantomJS')
 browser.get(url)
+print(browser.page_source)
 browser.quit()
